eee8cf6068717db5a55354d5703fd185cd79c64c
naga ruthvik <94837055+nagaruthvik@users.noreply.github.com>
2023-01-21 15:33:16 +0530

    Update Encapsulation.java

--- Encapsulation.java
+++ Encapsulation.java
@@ -4,12 +4,27 @@ package Cipherschool;
 class Amazon{
     float Transaction(float amt){
         Gpay gpayobj = new Gpay();
-        float totalAmount = amt+(amt*gpayobj.txnCharge);
+        gpayobj.setTxnCharge(0.07f);
+        float totalAmount = amt+(amt*gpayobj.getTxnCharge());
         return totalAmount;
     }
 }
-class Gpay{
-    float txnCharge=0.5f;
+class Gpay {
+    private float txnCharge = 0.5f;
+
+    public void setTxnCharge(float newCharge) {
+        if (newCharge <= 0.05f) {
+            System.out.println("Invalid TxtCharge");
+            return;
+        }
+        txnCharge = newCharge;
+
+    }
+
+    public float getTxnCharge() {
+        return txnCharge;
+
+    }
 }
 public class Encapsulation {
     public static void main(String[] args){
